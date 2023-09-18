b04998338d672ecc04e469919d1eab19701505e8
Marco Lasagna <marco.lasagna@studenti.unitn.it>
2023-09-18 15:14:56 +0200

    Aggiornata posizione file

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@
 #comandi bash con privilegi elevati, quindi, prima di eseguire questo script, l'utente si deve assicurare di poter eseguire
 #tali comandi.
 
-export GOOGLE_APPLICATION_CREDENTIALS="./src/main/application_default_credentials.json"
+export GOOGLE_APPLICATION_CREDENTIALS="./application_default_credentials.json"
 if [ ! -f $GOOGLE_APPLICATION_CREDENTIALS ]; then
   echo "Errore: file application_default_credentials.json non esistente.">&2;
 fi
@@ -14,4 +14,4 @@ fi
 #i file che identificano le firme.
 #sudo zip -d ./build/install/tirocinio/lib/tirocinio-1.0.jar 'META-INF/.SF' 'META-INF/.RSA' 'META-INF/*SF'
 
-java -jar ./build/install/tirocinio-shadow/lib/tirocinio-shadow-1.0.jar $1 $2 $3 $4
+java -jar ./tirocinio-shadow-1.0.jar $1 $2 $3 $4
