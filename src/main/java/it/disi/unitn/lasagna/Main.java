429d473169f47026522f1da76cd8f8dd357433f3
Marco Lasagna <marco.lasagna@studenti.unitn.it>
2024-05-19 23:29:27 +0200

    Aggiunta chiamata a metodo per inserire testo con font richiesto dall'utente all'interno dell'immagine

--- src/main/java/it/disi/unitn/lasagna/Main.java
+++ src/main/java/it/disi/unitn/lasagna/Main.java
@@ -103,8 +103,8 @@ public class Main {
                 System.out.println("WIDTH: " + width);
                 System.out.println("HEIGHT: " + height);
 
-                //Aggiungere la possibilità di selezionare il font tramite
-                //json2Image.getFont(name, style, size);
+                //Seleziona il font richiesto dall'utente.
+                json2Image.getFontFromJSON(parser);
                 json2Image.generate(directory, imageExt, width, height, 1800000);
 
                 AudioGenerator generator = new AudioGenerator(array);
